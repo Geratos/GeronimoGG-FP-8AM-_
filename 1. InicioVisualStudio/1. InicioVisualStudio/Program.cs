a4dae0e5dfcb7049cf3cb5f37ab0c1f16f22cad0
Geronimo Giraldo <geronimo.giraldo@upb.edu.co>
2025-08-01 09:30:36 -0500

    variables, constantes, tipos de datos

--- 1. InicioVisualStudio/1. InicioVisualStudio/Program.cs
+++ 1. InicioVisualStudio/1. InicioVisualStudio/Program.cs
@@ -10,6 +10,14 @@ namespace _1.InicioVisualStudio
     {
         static void Main(string[] args)
         {
+            //declaracion e inicializacion de variables
+            string nombre = "Geronimo";
+            const string iva = " 19% ";
+            string apellidos = "Giraldo Giraldo";
+            //no puedo cambiar el valor de iva pq es una constante
+            Console.Write(" pinga\n ");
+            Console.Write("\t" +   nombre + " " + apellidos + " " + iva);
+            Console.ReadKey();
         }
     }
 }
