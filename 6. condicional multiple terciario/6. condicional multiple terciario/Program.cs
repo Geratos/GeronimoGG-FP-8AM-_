73d238f89904d3e758383493f9a1cb06a6acccc6
Geronimo Giraldo <geronimo.giraldo@upb.edu.co>
2025-08-27 09:24:13 -0500

    parcial

---
+++ 6. condicional multiple terciario/6. condicional multiple terciario/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _6._condicional_multiple_terciario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float valorCompra = 0;
+            float descuento = 0;
+            char dia = ' ';
+
+            Console.WriteLine("Ingrese el valor de la compra:");
+            valorCompra=Single.Parse(Console.ReadLine()); //single.parse es para cuando se usa float
+            Console.WriteLine("Ingrese el día de la semana:l:Lunes, m:Martes, x:Miercoles, j:Jueves,v:Viernes, s:Sabado, d:Domingo");
+            dia=Convert.ToChar(Console.ReadLine());  //char es para poner 1 sola letra
+
+            switch (dia)
+            {
+                case 'l':
+                    descuento = valorCompra * 0.1f;
+                    Console.WriteLine("Hoy es lunes, descuento de " + descuento + ", valor total de la compra: " + (valorCompra - descuento));
+                    break;
+
+                case 'm':
+                    descuento = valorCompra * 0.15f;
+                    Console.WriteLine("Hoy es martes, descuento de " + descuento + ", valor total de la compra: " + (valorCompra - descuento));
+                    break;
+
+                case 'x':
+                    descuento = valorCompra * 0.1f;
+                    Console.WriteLine("Hoy es miercoles, descuento de " + descuento + ", valor total de la compra: " + (valorCompra - descuento));
+                    break;
+
+                case 'j':
+                    descuento = valorCompra * 0.15f;
+                    Console.WriteLine("Hoy es jueves, descuento de " + descuento + ", valor total de la compra: " + (valorCompra - descuento));
+                    break;
+
+                case 'v':
+                    descuento = valorCompra * 0.2f;
+                    Console.WriteLine("Hoy es viernes, descuento de " + descuento + ", valor total de la compra: " + (valorCompra - descuento));
+                    break;
+
+                case 's':
+                    descuento = valorCompra * 0.2f;
+                    Console.WriteLine("Hoy es sabado, descuento de " + descuento + ", valor total de la compra: " + (valorCompra - descuento));
+                    break;
+
+                case 'd':
+                    descuento = 0;
+                    Console.WriteLine($"Hoy es domingo, descuento de {descuento}, valor total de la compra: {valorCompra - descuento}");
+                    //esta es otra forma de ponerlo
+                    break;
+
+                default:
+                    Console.WriteLine("ingrese un dia valido no sea bobito");
+
+                    break;
+
+            }
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ Parcial_1/Parcial_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Parcial_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asignar Elementos por Mes y Signo zodiacal
+            Console.Write("Ingrese su mes de nacimiento:");
+            string mes = Console.ReadLine();
+
+            Console.Write("Ingrese su signo zodiacal:");
+            string signo = Console.ReadLine();
+
+            if ((mes == "marzo" || mes == "abril" || mes == "julio" || mes == "agosto" || mes == "noviembre" || mes == "diciembre")
+          && (signo == "aries" || signo == "leo" || signo == "sagitario"))
+                Console.WriteLine("Tu elemento es fuego :D");
+
+            else if ((mes == "abril" || mes == "mayo" || mes == "agosto" || mes == "septiembre" || mes == "diciembre" || mes == "enero")
+            && (signo == "tauro" || signo == "virgo" || signo == "capricornio"))
+                Console.WriteLine("Tu elemento es Tierra :D");
+
+            else if ((mes == "mayo" || mes == "junio" || mes == "septiembre" || mes == "octubre" || mes == "enero" || mes == "febrero")
+            && (signo == "géminis" || signo == "geminis" || signo == "libra" || signo == "acuario"))
+                Console.WriteLine("Tu elemento es Aire :D");
+
+            else if ((mes == "marzo" || mes == "junio" || mes == "julio" || mes == "octubre" || mes == "noviembre" || mes == "febrero")
+            && (signo == "cáncer" || signo == "cancer" || signo == "escorpión" || signo == "escorpion" || signo == "acuario"))
+                Console.WriteLine("Tu elemento es Agua :D");
+            //algunos están repetidos por si lo pone sin tilde
+            else Console.WriteLine();
+
+        }
+    }
+}
