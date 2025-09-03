2840061802e54bd4b3e2ae61cb857e829718acc2
Geronimo Giraldo <geronimo.giraldo@upb.edu.co>
2025-09-03 09:18:52 -0500

    While

--- 13. While5/13. While5/Program.cs
+++ 13. While5/13. While5/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*13. Realizar un algoritmo que le pida al usuario ingresar por teclado números enteros al azar. Al usuario le corresponde ingresar la cantidad de números que va a introducir.  
+            El algoritmo debe escribir en pantalla: 
+                La cantidad de números introducidos que son mayores que 0 
+                La cantidad de números introducidos menores que 0  
+                La cantidad de números iguales a 0. */
+
+            int numero = 0;
+
+            Console.WriteLine("Ingrese los numeros que quiera (presionar la tecla I para acabar):");
+            numero = int.Parse(Console.ReadLine());
+            
         }
     }
 }
---
+++ 13. While6/13. While6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _13._While6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //14. Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.
+
+            int rango = 0;
+            int contadorPares = 0;
+            int contador = 1;
+
+            Console.WriteLine("ingrese el rango a evaluar");
+            rango=int.Parse(Console.ReadLine());
+
+            while(contador <= rango)
+
+            {
+                if (contador%2==0)
+                {
+                    contadorPares++;
+                }
+
+                contador++;
+            }
+            Console.WriteLine($"la cantidad de números pares de 1 hasta {rango} es: {contadorPares}"); 
+        }
+    }
+}
