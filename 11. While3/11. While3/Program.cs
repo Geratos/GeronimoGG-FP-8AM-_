7d0fa714ab610336257c7bc9c891094199d9d9d6
Geronimo Giraldo <geronimo.giraldo@upb.edu.co>
2025-09-03 08:30:22 -0500

    suma a 99

---
+++ 11. While3/11. While3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _11._While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo.
+            Se debe mostrar por pantalla el total de la suma de los números ingresados. */
+
+            int numero;
+            int suma = 0;
+            
+            
+        }
+    }
+}
---
+++ 12. While4/While3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar algorito que permita obtener la suma de todos los números impares hasta el 99
+            int contador = 1;
+            int suma = 0;
+
+            while (contador <= 99)
+            {
+                suma += contador; 
+                Console.WriteLine($"{suma - contador} + {contador} = {suma}");
+                contador += 2;
+
+                Console.WriteLine("-------------------------------------------------------------------");
+            }
+            Console.WriteLine($"La suma de los números impares hasta el 99 es: {suma}"); 
+        }
+    }
+}
